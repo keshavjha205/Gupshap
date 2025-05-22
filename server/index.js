f331c32e0c5de6c57b405194aaa11978bb48b31c
keshavjha205 <keshavjha1081@gmail.com>
2025-05-22 10:30:51 +0530

    socket implementation

--- server/index.js
+++ server/index.js
@@ -5,10 +5,11 @@ import authRoutes from './src/routes/auth.route.js'
 import messageRoutes from './src/routes/message.route.js'
 import cookieParser from "cookie-parser"
 import cors from "cors"
+import { app, server } from "./src/lib/socket.js"
 
 dotenv.config()
 
-const app = express();
+
 
 const PORT = process.env.PORT;
 
@@ -23,7 +24,7 @@ app.use(cors({
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
     connectDB();
 })
