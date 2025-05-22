3090f8626fb79c93e7808ffe090d50ec59bc8a9f
keshavjha205 <keshavjha1081@gmail.com>
2025-05-22 10:14:13 +0530

    chat conatiner

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import { Loader } from 'lucide-react'
 import {Toaster} from 'react-hot-toast'
 import { useThemeStore } from './store/useThemeStore'
 
+
 function App() {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
   const {theme} = useThemeStore();
--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -4,6 +4,7 @@ import ChatHeader from './ChatHeader'
 import MessageInput from './MessageInput'
 import MessageSkeleton from './skeleton/MessageSkeleton'
 import { useAuthStore } from '../store/useAuthStore'
+import { formatMessageTime } from '../lib/utils'
 
 function ChatContainer() {
  const {messages, getMessages, isMessagesLoading, selectedUser } = useChatStore();
@@ -31,6 +32,7 @@ if(isMessagesLoading) {
 
        <div className='flex-1 overflow-y-auto p-4 space-y-4'>
           {messages.map((message) => {
+            return(
             <div key={message._id} 
             className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}>
                 <div className='chat-image avatar'>
@@ -44,11 +46,18 @@ if(isMessagesLoading) {
                 </div>
                       <div className='chat-header mb-1'>
                         <time className='text-xs opacity-50 ml-1'>
-                          {message.createdAt}
+                          {formatMessageTime(message.createdAt)}
                         </time>
                       </div>
-                      
-            </div>
+                      <div className='chat-bubble flex flex-col'>
+                        {message.image && (
+                          <img src={message.image} alt="Attachment" 
+                          className='sm:max-w-[200px] rounded-md mb-2'/>
+                        )}
+                        {message.text && <p>{message.text}</p>}
+                      </div>
+            </div> 
+            )
           })
 
           }
